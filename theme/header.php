0b609e19bd0ce85939127d3014de23f85648f8db
augustinDuval <duvalaugustin@gmail.com>
2017-12-22 10:35:47 +0100

    Google verification tag

--- theme/header.php
+++ theme/header.php
@@ -48,7 +48,7 @@
     <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/share-fb.png">
     <meta property="og:description" content="<?php echo $og_description; ?>">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="google-site-verification" content="bTnS-WgN8k5eTpzBbN6ATibb2BdN1g8qZ-DEOVtYB5s" />
+    <meta name="google-site-verification" content="6EHu9r95V7L27wF49pEYrAovlGC0JPlECZ93h90Z0l0" />
 
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/responsive.css">
@@ -58,6 +58,18 @@
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
 	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js" type="text/javascript"></script>
+	
+	<!-- Analytics Code -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-68872992-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+
 </head>
 
 <body <?php body_class(); ?>>
