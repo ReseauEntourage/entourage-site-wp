8b5cbff8aa6049e9c4d733056879106826d7421f
gregclermont <580609+gregclermont@users.noreply.github.com>
2019-09-02 15:08:20 +0200

    remove hardcoded Facebook Pixel

--- theme/page-app.php
+++ theme/page-app.php
@@ -656,22 +656,6 @@
         ga('send', 'pageview');
     </script>
 
-    <!-- Facebook Pixel Code -->
-    <script type='text/javascript'>
-        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-        document,'script','https://connect.facebook.net/en_US/fbevents.js');
-
-        fbq('init', '230874854501308');
-        fbq('track', 'PageView');
-    </script>
-    <noscript>
-        <img height="1" width="1" style="display:none" alt="fbpx" src="https://www.facebook.com/tr?id=230874854501308&ev=PageView&noscript=1" />
-    </noscript>
-    <!-- End Facebook Pixel Code -->
-
     <!-- Facebook JS SDK -->
     <script>
         window.fbAsyncInit = function() {
