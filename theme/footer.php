80488a61e890f0c548a26fa4d8669c3b3b98bfe7
gregclermont <580609+gregclermont@users.noreply.github.com>
2019-09-02 14:08:34 +0200

    setup new facebook pixel

--- theme/footer.php
+++ theme/footer.php
@@ -33,26 +33,6 @@
 
 <script src="<?php asset_url('js/script.js'); ?>" type="text/javascript"></script>
 
-<!-- Facebook Pixel Code -->
-<script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window,document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    
-    fbq('init', '1977352069219759'); 
-    fbq('track', 'PageView');
-</script>
-<noscript>
-	<img height="1" width="1" 
-src="https://www.facebook.com/tr?id=1977352069219759&ev=PageView
-&noscript=1"/>
-</noscript>
-
 <!-- Load Facebook SDK for JavaScript -->
 <script>
     window.fbAsyncInit = function() {
--- theme/page-app.php
+++ theme/page-app.php
@@ -646,22 +646,20 @@
     </script>
 
     <!-- Facebook Pixel Code -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window,document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        
-        fbq('init', '1977352069219759'); 
+    <script type='text/javascript'>
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+        document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', '230874854501308');
         fbq('track', 'PageView');
     </script>
     <noscript>
-        <img height="1" width="1" src="https://www.facebook.com/tr?id=1977352069219759&ev=PageView&noscript=1"/>
+        <img height="1" width="1" style="display:none" alt="fbpx" src="https://www.facebook.com/tr?id=230874854501308&ev=PageView&noscript=1" />
     </noscript>
+    <!-- End Facebook Pixel Code -->
 
     <!-- Facebook JS SDK -->
     <script>
